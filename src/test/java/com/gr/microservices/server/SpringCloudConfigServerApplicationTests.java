e8d4f1300a948bf08b8cab8847141870b4237ae4
Giri Babu <gbabu@flatironssolutions.com>
2021-11-15 10:38:47 +0530

    Initial Commit

---
+++ src/test/java/com/gr/microservices/server/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr.microservices.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
